4c6526669ad9194aa908f618b99637184c72f881
Thayna Corbicelis <tcorbicelis@gmail.com>
2026-03-20 13:00:15 -0300

    home

---
+++ script.js
@@ -0,0 +1,6 @@
+ function openModal() {
+    document.getElementById('loginModal').style.display = 'flex';
+  }
+  function closeModal() {
+    document.getElementById('loginModal').style.display = 'none';
+  }
